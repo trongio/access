f813ef67bcc6706981bae7b377201a011400d329
trongio <giokakabadze50@gmail.com>
2021-02-15 15:27:57 +0400

    Create login.blade.php

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<div class="container">
+    <h1>Login</h1>
+    <p id="serverResponse"></p>
+
+    <div class="form-group">
+        <label for="email">Email address:</label>
+        <input id="email" type="email" class="form-control "
+               name="email">
+    </div>
+
+    <div class="form-group">
+        <label for="password">password:</label>
+        <input id="password" type="password" class="form-control "
+               name="password">
+    </div>
+
+    <button id="submitbutton" name="signup-btn" type="submit" class="btn btn-primary">Submit</button>
+
+</div>
+
+</body>
+</html>
